e60fcef12480b6c5bf06877169d462f901eea616
JoaoAlisonTI <joaoalison.ti@gmail.com>
2024-04-12 17:25:19 -0300

    feat: Adicionando component Table e seus subcomponents; Adicionando component IconButton para personalização dinâmica dos botões assim como o component NavLink para personalização dinâmica dos links; Adicionando as bibliotecas dayjs e faker-js para criar um data fake para testes; Adicionando as funcionalidades de listagem na tabela;

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import uniteIcon from "../assets/unite-icon.svg"
+import { NavLink } from "./NavLink"
 
 export function Header() {
   return (
     <header className="flex items-center gap-5 py-2">
       <img alt="Icon nlw unite" src={uniteIcon} />
       <nav className="flex items-center gap-5">
-        <a href="#" className="font-medium text-sm text-zinc-300">Eventos</a>
-        <a href="#" className="font-medium text-sm">Participantes</a>
+        <NavLink href="/eventos">Eventos</NavLink>
+        <NavLink href="/participantes">Participantes</NavLink>
       </nav>
     </header>
   )
