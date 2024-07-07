29cb8af9841cd3d6660b6016c06633e983d1a495
JoaoAlisonTI <joaoalison.ti@gmail.com>
2024-07-07 17:13:51 -0300

    fix: Corrigindo erro no deploy alterando o caminho da url do icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "./NavLink"
 export function Header() {
   return (
     <header className="flex items-center gap-5 py-2">
-      <img alt="Icon nlw unite" src="../assets/unite-icon.svg" />
+      <img alt="Icon nlw unite" src="../src/assets/unite-icon.svg" />
       <nav className="flex items-center gap-5">
         <NavLink href="/eventos">Eventos</NavLink>
         <NavLink href="/participantes">Participantes</NavLink>
